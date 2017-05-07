3eec2a586d2c2a90e1c411e1b1851929192d5ccd
Steffen Hankiewicz <steffen.hankiewicz@intranda.com>
2017-05-08 01:32:10 +0200

    Import of metadata and copying images and pdf works so far

--- goobi-plugin-import-bsz/src/de/intranda/goobi/plugins/ImportBodenseeZeitschriften_Kultur.java
+++ goobi-plugin-import-bsz/src/de/intranda/goobi/plugins/ImportBodenseeZeitschriften_Kultur.java
@@ -1,6 +1,8 @@
 package de.intranda.goobi.plugins;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
@@ -8,8 +10,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedHashSet;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.goobi.production.enums.ImportReturnValue;
 import org.goobi.production.enums.ImportType;
 import org.goobi.production.enums.PluginType;
@@ -22,6 +26,12 @@ import org.goobi.production.plugin.interfaces.IOpacPlugin;
 import org.goobi.production.plugin.interfaces.IPlugin;
 import org.goobi.production.properties.ImportProperty;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonReader;
+
+import de.intranda.goobi.plugins.bsz.BSZ_Kultur_Element;
+import de.sub.goobi.config.ConfigurationHelper;
 import de.sub.goobi.forms.MassImportForm;
 import de.sub.goobi.helper.exceptions.ImportPluginException;
 import de.unigoettingen.sub.search.opac.ConfigOpac;
@@ -36,7 +46,9 @@ import ugh.dl.Metadata;
 import ugh.dl.Prefs;
 import ugh.exceptions.MetadataTypeNotAllowedException;
 import ugh.exceptions.PreferencesException;
+import ugh.exceptions.TypeNotAllowedAsChildException;
 import ugh.exceptions.TypeNotAllowedForParentException;
+import ugh.exceptions.UGHException;
 import ugh.exceptions.WriteException;
 import ugh.fileformats.mets.MetsMods;
 
@@ -44,21 +56,41 @@ import ugh.fileformats.mets.MetsMods;
 @Log4j
 public class ImportBodenseeZeitschriften_Kultur implements IImportPlugin, IPlugin {
 	private static final String PLUGIN_NAME = "ImportBodenseeZeitschriften_Kultur";
+
+	private static final String IMPORT_FOLDER = "/Users/steffen/Desktop/BSZ/";
+	private static final String IMAGE_FOLDER_EXTENSION = "_" + ConfigurationHelper.getInstance().getMediaDirectorySuffix();
+	private static final String IMAGE_FILE_PREFIX_TO_REMOVE = "/data/kebweb/kult/";
+	//SQL converted to JSON simply using http://codebeautify.org/sql-to-json-converter
+	private static final String IMPORT_FILE = IMPORT_FOLDER + "import_kultur.json";
+	
 	private MassImportForm form;
 	private Prefs prefs;
-	private static final File ROOT_FOLDER = new File("/Users/steffen/Desktop/BSZ/");
 	private String data = "";
 	private String importFolder = "";
 	private String ats = "kultur";
-	private String currentIdentifier = "";
 	private String catalogue = "BSZ-BW";
 	private String ppn = "310869048";
-
+	private String ppn_volume;
+	
 	@Override
-	public String getProcessTitle() {
-		return ats + "_" + currentIdentifier;
+	public List<String> getAllFilenames() {
+		// read json file to list all years
+		LinkedHashSet<String> years = new LinkedHashSet<String>();
+		try {
+			JsonReader reader = new JsonReader(new FileReader(IMPORT_FILE));
+			Gson gson = new GsonBuilder().create();
+			reader.beginArray();
+			while (reader.hasNext()) {
+				BSZ_Kultur_Element element = gson.fromJson(reader, BSZ_Kultur_Element.class);
+				years.add(element.getJahr());
+			}
+			reader.close();
+		} catch (IOException e) {
+			log.error("Problem occured while reading the json file for kultur import", e);
+		}
+		return new ArrayList<String>(years);
 	}
-
+	
 	@Override
 	public List<ImportObject> generateFiles(List<Record> records) {
 		List<ImportObject> answer = new ArrayList<ImportObject>();
@@ -68,14 +100,12 @@ public class ImportBodenseeZeitschriften_Kultur implements IImportPlugin, IPlugi
 			// updatae the progress bar for each record
 			form.addProcessToProgressBar();
 			
-			// generate an ImportObject for each Record and add it to the result list later on
-			ImportObject io = new ImportObject();
-			currentIdentifier = record.getId();
-			//String folder = ROOT_FOLDER.getAbsolutePath() + File.separator + currentIdentifier;
-			String metsFileName = getImportFolder() + currentIdentifier + ".xml";
-			io.setMetsFilename(metsFileName);
-			io.setProcessTitle(getProcessTitle());
-			
+			// generate an ImportObject for each selected year and add it to the result list later on
+			ImportObject importObjectYear = new ImportObject();
+			ppn_volume = ppn + "_" + record.getId();
+			String metsFileName = getImportFolder() + ppn_volume + ".xml";
+			importObjectYear.setMetsFilename(metsFileName);
+			importObjectYear.setProcessTitle("kultur_" + ppn_volume);
 			try {
 				// request the object from the catalogue and generate a FileFormat
 				Fileformat fileformat = convertData();
@@ -87,35 +117,44 @@ public class ImportBodenseeZeitschriften_Kultur implements IImportPlugin, IPlugi
 						DocStructType docstructBoundBook = prefs.getDocStrctTypeByName("BoundBook");
 						DocStruct physical = dd.createDocStruct(docstructBoundBook);
 						Metadata pathimagefiles = new Metadata(prefs.getMetadataTypeByName("pathimagefiles"));
-						pathimagefiles.setValue(currentIdentifier);
+						pathimagefiles.setValue(ppn_volume);
 						physical.addMetadata(pathimagefiles);
 						dd.setPhysicalDocStruct(physical);
+					
 					} catch (PreferencesException | TypeNotAllowedForParentException
 					        | MetadataTypeNotAllowedException e1) {
 						log.error("Errow while doing the massimport in ImportBodenseeZeitschriften_Kultur", e1);
 					}
 					
-					// write Mets file into temp folder of Goobi to let it be imported afterwards
 					try {
+						// add all issues and the correct pages there
+						addAllIssues(fileformat, record.getId(),importObjectYear.getProcessTitle());
+						
+						// write Mets file into temp folder of Goobi to let it be imported afterwards
 						MetsMods mm = new MetsMods(this.prefs);
 						mm.setDigitalDocument(fileformat.getDigitalDocument());
 						log.debug("Writing '" + metsFileName + "' into given folder...");
 						mm.write(metsFileName);
 						
-						io.setImportReturnValue(ImportReturnValue.ExportFinished);
-					} catch (PreferencesException e) {
-						log.error("PreferencesException during the massimport in ImportBodenseeZeitschriften_Kultur", e);
-						io.setErrorMessage(e.getMessage());
-						io.setImportReturnValue(ImportReturnValue.InvalidData);
+						importObjectYear.setImportReturnValue(ImportReturnValue.ExportFinished);
+					} catch (IOException e) {
+						log.error("IOException during the massimport in ImportBodenseeZeitschriften_Kultur", e);
+						importObjectYear.setErrorMessage(e.getMessage());
+						importObjectYear.setImportReturnValue(ImportReturnValue.InvalidData);
 					} catch (WriteException e) {
 						log.error("WriteException during the massimport in ImportBodenseeZeitschriften_Kultur", e);
-						io.setErrorMessage(e.getMessage());
-						io.setImportReturnValue(ImportReturnValue.WriteError);
+						importObjectYear.setErrorMessage(e.getMessage());
+						importObjectYear.setImportReturnValue(ImportReturnValue.WriteError);
+					} catch (UGHException e) {
+						log.error("PreferencesException during the massimport in ImportBodenseeZeitschriften_Kultur", e);
+						importObjectYear.setErrorMessage(e.getMessage());
+						importObjectYear.setImportReturnValue(ImportReturnValue.InvalidData);
 					}
 				} else {
-					io.setImportReturnValue(ImportReturnValue.InvalidData);
+					importObjectYear.setImportReturnValue(ImportReturnValue.InvalidData);
 				}
-				answer.add(io);
+				
+				answer.add(importObjectYear);
 			} catch (ImportPluginException e) {
 				log.error("ImportPluginException during the massimport in ImportBodenseeZeitschriften_Kultur", e);
 			}
@@ -123,6 +162,64 @@ public class ImportBodenseeZeitschriften_Kultur implements IImportPlugin, IPlugi
 		return answer;
 	}
 
+	
+	/**
+	 * add all issues and pages to the volume
+	 * @param ff
+	 */
+	private void addAllIssues(Fileformat ff, String inYear, String inProcessTitle) throws IOException, UGHException{
+		File targetFolderImages = new File(getImportFolder() + ppn_volume + File.separator + "images" 
+        		+ File.separator + inProcessTitle + IMAGE_FOLDER_EXTENSION);
+		File targetFolderPdf = new File(getImportFolder() + ppn_volume + File.separator + "ocr" 
+        		+ File.separator);
+        targetFolderImages.mkdirs();
+		targetFolderPdf.mkdirs();
+		
+		DocStruct volume = ff.getDigitalDocument().getLogicalDocStruct().getAllChildren().get(0);
+		DocStructType issueType = prefs.getDocStrctTypeByName("PeriodicalIssue");
+		DocStruct issue = null;
+		String lastIssue = "";
+		
+		JsonReader reader = new JsonReader(new FileReader(IMPORT_FILE));
+		Gson gson = new GsonBuilder().create();
+		reader.beginArray();
+		while (reader.hasNext()) {
+			BSZ_Kultur_Element element = gson.fromJson(reader, BSZ_Kultur_Element.class);
+			// add all issues and pages from one year to this volume
+			if (element.getJahr().equals(inYear)){
+				
+				// create new issue docstruct if necessary
+				if (!lastIssue.equals(element.getBookletid())){
+					lastIssue = element.getBookletid();
+					issue = ff.getDigitalDocument().createDocStruct(issueType);
+					
+					// add title to issue
+					Metadata issueTitleMd = new Metadata(prefs.getMetadataTypeByName("TitleDocMain"));
+					issueTitleMd.setValue(element.getBookletid().substring(element.getBookletid().lastIndexOf(".")));
+					issue.addMetadata(issueTitleMd);
+					volume.addChild(issue);
+					
+					// copy pdf file into right place in tmp folder
+					File pdfFile = new File(IMPORT_FOLDER, element.getPageid().substring(0, element.getPageid().lastIndexOf("_")) + ".pdf");
+					if (pdfFile.exists()){
+						FileUtils.copyFile(pdfFile, new File(targetFolderPdf, inProcessTitle + ".pdf"));
+					}
+				}
+				
+				// copy each image into right place in tmp folder
+				File imageFile = new File(IMPORT_FOLDER, element.getJpg().substring(IMAGE_FILE_PREFIX_TO_REMOVE.length() -1));
+				log.info("copy image from " + imageFile.getAbsolutePath() + " to " + targetFolderImages.getAbsolutePath());
+				FileUtils.copyFile(imageFile, new File(targetFolderImages, imageFile.getName()));
+				
+				// no matter if new or current issue, add now all pages to current issue
+				if (issue!=null){
+					
+				}
+			}
+		}
+		reader.close();
+	}
+	
 	@Override
     public Fileformat convertData() throws ImportPluginException {
 		try {
@@ -145,11 +242,11 @@ public class ImportBodenseeZeitschriften_Kultur implements IImportPlugin, IPlugi
 			md.setValue(ppn);
 			ds.addMetadata(md);
 			
-			// assign a ppn digital to the child docstruct
+			// assign a ppn digital to the child docstruct (volume)
 			if (ds.getType().isAnchor()) {
 				DocStruct child = ds.getAllChildren().get(0);
 				Metadata md2 = new Metadata(prefs.getMetadataTypeByName("CatalogIDDigital"));
-				md2.setValue(ppn + "0001");
+				md2.setValue(ppn_volume);
 				child.addMetadata(md2);
 			}
 			
@@ -160,21 +257,23 @@ public class ImportBodenseeZeitschriften_Kultur implements IImportPlugin, IPlugi
 		return null;
 	}
 	
-	@Override
-	public List<String> getAllFilenames() {
-		List<String> folderNames = new ArrayList<String>();
-		try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(ROOT_FOLDER.getAbsolutePath()))) {
-			for (Path entry : stream) {
-				if (Files.isDirectory(entry)) {
-					folderNames.add(entry.toFile().getName());
-				}
-			}
-		}catch (Exception e) {
-			log.error("Error listing all folders in import root", e);
-		}
-		Collections.sort(folderNames);
-		return folderNames;
-	}
+//	private List<BSZ_Kultur_Element> getKulturElements(){
+//		Gson gson = new Gson();
+//		BufferedReader br = new BufferedReader(new FileReader(IMPORT_FILE));
+////		Type type = new TypeToken<List<Model>>(){}.getType();
+//		List<BSZ_Kultur_Element> elements = gson.fromJson(br, List<BSZ_Kultur_Element>);
+//		
+//		
+////		try(Reader reader = new InputStreamReader(JsonToJava.class.getResourceAsStream("/Server1.json"), "UTF-8")){
+////            
+////        	Gson gson = new GsonBuilder().create();
+////            Person p = gson.fromJson(reader, Person.class);
+////            System.out.println(p);
+////        }
+//	}
+	
+	
+
 
 	@Override
 	public List<Record> generateRecordsFromFilenames(List<String> filenames) {
@@ -288,6 +387,11 @@ public class ImportBodenseeZeitschriften_Kultur implements IImportPlugin, IPlugi
 	public void setDocstruct(DocstructElement dse) {
 	}
 
+	@Override
+	public String getProcessTitle() {
+		return ats;
+	}
+	
 	// public static void main(String[] args) {
 	//
 	// String path = "/home/robert/Downloads/bsz/007140975";
---
+++ goobi-plugin-import-bsz/src/de/intranda/goobi/plugins/bsz/BSZ_Kultur_Element.java
@@ -0,0 +1,14 @@
+package de.intranda.goobi.plugins.bsz;
+
+import lombok.Data;
+
+@Data
+public class BSZ_Kultur_Element {
+	private String pageid;
+	private String bookletid;
+	private String journalid;
+	private String lfnr;
+	private String jahr;
+	private String label;
+	private String jpg;
+}
