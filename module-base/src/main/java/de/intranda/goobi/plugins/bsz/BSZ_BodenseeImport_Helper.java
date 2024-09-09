602de26f85dc31835a5f587de8984f319bbb6189
Florian Alpers <florian.apers@intranda.com>
2024-09-09 09:23:48 +0200

    update code for pdfbox

--- module-base/src/main/java/de/intranda/goobi/plugins/bsz/BSZ_BodenseeImport_Helper.java
+++ module-base/src/main/java/de/intranda/goobi/plugins/bsz/BSZ_BodenseeImport_Helper.java
@@ -12,6 +12,7 @@ import java.util.List;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.SystemUtils;
+import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.goobi.production.enums.ImportReturnValue;
@@ -63,7 +64,7 @@ public class BSZ_BodenseeImport_Helper {
     private boolean createIssues = true;
     private boolean separateBookletIds = false;
 
-    public BSZ_BodenseeImport_Helper(String inBasicName, boolean inSeparateBooketIds){
+    public BSZ_BodenseeImport_Helper(String inBasicName, boolean inSeparateBooketIds) {
         basic_name = inBasicName;
         image_file_prefix_to_remove = "/data/kebweb/" + basic_name + "/";
         bsz_import_sql_file = basic_folder + basic_name + ".sql";
@@ -79,8 +80,7 @@ public class BSZ_BodenseeImport_Helper {
     }
 
     /**
-     * Read all years from the given JSON file to pass back an ordered list of all
-     * importable years for the mass import plugin GUI
+     * Read all years from the given JSON file to pass back an ordered list of all importable years for the mass import plugin GUI
      * 
      * @return List<String> of years
      */
@@ -89,19 +89,19 @@ public class BSZ_BodenseeImport_Helper {
         LinkedHashSet<String> years = new LinkedHashSet<>();
 
         try {
-            File f = new File (bsz_import_sql_file);
+            File f = new File(bsz_import_sql_file);
             List<String> lines = FileUtils.readLines(f, "UTF-8");
             for (String line : lines) {
-                if (line.startsWith("INSERT INTO")){
+                if (line.startsWith("INSERT INTO")) {
                     BSZ_BodenseeImport_Element element = convertSqlToElement(line);
 
                     String isn = "";
-                    if (StringUtils.isNumeric(element.getIssueNumber())){
+                    if (StringUtils.isNumeric(element.getIssueNumber())) {
                         isn = "_" + element.getIssueNumber();
                     }
-                    if (separateBookletIds){
+                    if (separateBookletIds) {
                         years.add(element.getJahr() + isn + "_" + element.getBookletid());
-                    } else{
+                    } else {
                         years.add(element.getJahr() + isn);
                     }
                 }
@@ -121,7 +121,7 @@ public class BSZ_BodenseeImport_Helper {
      * @param inline the insert statement line of sql to parse
      * @return a new {@link BSZ_BodenseeImport_Element}
      */
-    private BSZ_BodenseeImport_Element convertSqlToElement(String inline){
+    private BSZ_BodenseeImport_Element convertSqlToElement(String inline) {
         String line = inline.substring(inline.indexOf("VALUES (") + 8, inline.lastIndexOf(")"));
         String[] parts = line.split(",");
         BSZ_BodenseeImport_Element element = new BSZ_BodenseeImport_Element();
@@ -136,11 +136,9 @@ public class BSZ_BodenseeImport_Helper {
     }
 
     /**
-     * Generates {@link ImportObject} element for given {@link Record} and store it in the list of
-     * {@link ImportObject} to be imported then afterwards
-     * This method is the main entry to to the catalogue import of bibliographic data, the generation
-     * of a {@link Fileformat} and to enrich this with more bibliographic data, to read the logical
-     * structure from the JSON file and assign pages with page labes to these structural elements
+     * Generates {@link ImportObject} element for given {@link Record} and store it in the list of {@link ImportObject} to be imported then afterwards
+     * This method is the main entry to to the catalogue import of bibliographic data, the generation of a {@link Fileformat} and to enrich this with
+     * more bibliographic data, to read the logical structure from the JSON file and assign pages with page labes to these structural elements
      * 
      * @param answer List of {@link ImportObject} where to add the generated {@link ImportObject} to
      * @param record Record with information about the item to do the import for
@@ -180,7 +178,7 @@ public class BSZ_BodenseeImport_Helper {
 
                 try {
                     // add all issues and the correct pages there
-                    addAllIssues(fileformat, record.getId(),importObjectYear.getProcessTitle());
+                    addAllIssues(fileformat, record.getId(), importObjectYear.getProcessTitle());
 
                     // write Mets file into temp folder of Goobi to let it be imported afterwards
                     MetsMods mm = new MetsMods(prefs);
@@ -213,9 +211,8 @@ public class BSZ_BodenseeImport_Helper {
     }
 
     /**
-     * Method to do the catalogue request for a given identifier and to generate a {@link Fileformat} out of it
-     * During the creation of the {@link Fileformat} it is enriched with an updated identifier, a viewer
-     * sub theme and the right digital collection assignements
+     * Method to do the catalogue request for a given identifier and to generate a {@link Fileformat} out of it During the creation of the
+     * {@link Fileformat} it is enriched with an updated identifier, a viewer sub theme and the right digital collection assignements
      * 
      * @return {@link Fileformat} to use it as METS file afterwards
      * @throws ImportPluginException
@@ -259,7 +256,7 @@ public class BSZ_BodenseeImport_Helper {
      * @throws IOException
      * @throws COSVisitorException
      */
-    private void extractPdf(String inYear, String inProcessTitle, List<BSZ_BodenseeImport_Element> elements) throws IOException{
+    private void extractPdf(String inYear, String inProcessTitle, List<BSZ_BodenseeImport_Element> elements) throws IOException {
         File targetFolderPdfSingles = new File(tempFolder + ppn_volume + File.separator + "ocr"
                 + File.separator + inProcessTitle + "_pdf");
         targetFolderPdfSingles.mkdirs();
@@ -268,11 +265,11 @@ public class BSZ_BodenseeImport_Helper {
 
         List<Path> allMyFiles = new ArrayList();
         //		allMyFiles = NIOFileUtils.listFiles(bsz_import_folder,PDF_FILTER);
-        if (elements.size()>0){
-            String firstImage = elements.get(0).getJpg().substring(image_file_prefix_to_remove.length() -1);
+        if (elements.size() > 0) {
+            String firstImage = elements.get(0).getJpg().substring(image_file_prefix_to_remove.length() - 1);
             firstImage = firstImage.substring(0, firstImage.lastIndexOf("/"));
             File folderPath = new File(bsz_import_folder, firstImage);
-            allMyFiles.addAll(StorageProvider.getInstance().listFiles(folderPath.getAbsolutePath(),PDF_FILTER));
+            allMyFiles.addAll(StorageProvider.getInstance().listFiles(folderPath.getAbsolutePath(), PDF_FILTER));
         }
         for (Path entry : allMyFiles) {
             log.debug(entry.toAbsolutePath());
@@ -308,7 +305,7 @@ public class BSZ_BodenseeImport_Helper {
                     f.getAbsolutePath().contains("/jall/") ||
                     f.getAbsolutePath().contains("/vgeb/")) {
                 // create single page pdf files
-                PDDocument inputDocument = PDDocument.load(f);
+                PDDocument inputDocument = Loader.loadPDF(f);
                 for (int page = 1; page <= inputDocument.getNumberOfPages(); ++page) {
                     PDPage pdPage = inputDocument.getDocumentCatalog().getPages().get(page - 1);
                     PDDocument outputDocument = new PDDocument();
@@ -327,17 +324,16 @@ public class BSZ_BodenseeImport_Helper {
         public boolean accept(Path path) {
             boolean fileOk = false;
             String name = path.getFileName().toString();
-            if (name.toLowerCase().endsWith(".pdf")){
+            if (name.toLowerCase().endsWith(".pdf")) {
                 fileOk = true;
             }
             return fileOk;
         }
     };
 
-
     /**
-     * Method to add all issues into the the generated {@link Fileformat} incl. page labels, structural sub elements
-     * and page assignements. In case a pdf file exists for a the volume too it is extracted into individual pages
+     * Method to add all issues into the the generated {@link Fileformat} incl. page labels, structural sub elements and page assignements. In case a
+     * pdf file exists for a the volume too it is extracted into individual pages
      * 
      * @param ff {@link Fileformat} to use for the enrichtment
      * @param inYearAndIssueNumber the year to use for the extraction of data from the JSON result
@@ -359,13 +355,12 @@ public class BSZ_BodenseeImport_Helper {
         String lastIssue = "";
         int physicalPageNumber = 1;
 
-
-        if (createIssues){
+        if (createIssues) {
             ff.getDigitalDocument().getLogicalDocStruct().getAllChildren().remove(volume);
-        }else{
+        } else {
             String justYear = inYearAndIssueNumber;
-            if (justYear.contains("_")){
-                justYear=justYear.substring(0,justYear.indexOf("_"));
+            if (justYear.contains("_")) {
+                justYear = justYear.substring(0, justYear.indexOf("_"));
             }
             // add title to volume
             addMetadata(volume, "TitleDocMain", title + " " + justYear);
@@ -381,7 +376,6 @@ public class BSZ_BodenseeImport_Helper {
             addMetadata(volume, "PublicationYear", justYear);
         }
 
-
         DocStruct logicalDocstruct = ff.getDigitalDocument().getLogicalDocStruct();
         if (logicalDocstruct.getType().isAnchor()) {
             if (logicalDocstruct.getAllChildren() != null && logicalDocstruct.getAllChildren().size() > 0) {
@@ -393,13 +387,13 @@ public class BSZ_BodenseeImport_Helper {
         List<BSZ_BodenseeImport_Element> elements = readElementsFromSql(inYearAndIssueNumber);
 
         // extract given pdf file
-        extractPdf(inYearAndIssueNumber,inProcessTitle, elements);
+        extractPdf(inYearAndIssueNumber, inProcessTitle, elements);
 
         for (BSZ_BodenseeImport_Element element : elements) {
 
             String issueForTitle = "";
             String issueForDate = "01";
-            if (StringUtils.isNumeric(element.getIssueNumber())){
+            if (StringUtils.isNumeric(element.getIssueNumber())) {
                 issueForTitle = "-" + element.getIssueNumber();
                 issueForDate = element.getIssueNumber();
             }
@@ -407,7 +401,7 @@ public class BSZ_BodenseeImport_Helper {
             String year = element.getJahr();
 
             // create new issue docstruct if necessary
-            if (!lastIssue.equals(element.getBookletid())){
+            if (!lastIssue.equals(element.getBookletid())) {
                 lastIssue = element.getBookletid();
                 issue = ff.getDigitalDocument().createDocStruct(issueType);
 
@@ -423,20 +417,20 @@ public class BSZ_BodenseeImport_Helper {
                 addMetadata(issue, "ViewerSubTheme", "bsz-st-bodenseebibliotheken");
                 // add issue to volume
                 volume.addChild(issue);
-                if (createIssues){
+                if (createIssues) {
                     adaptIdentifier(issue, "CatalogIDDigital", ppn_volume);
                     ff.getDigitalDocument().getLogicalDocStruct().addChild(issue);
                 }
             }
 
             // copy each image into right place in tmp folder
-            File imageFile = new File(bsz_import_folder, element.getJpg().substring(image_file_prefix_to_remove.length() -1));
+            File imageFile = new File(bsz_import_folder, element.getJpg().substring(image_file_prefix_to_remove.length() - 1));
             log.debug("copy image from " + imageFile.getAbsolutePath() + " to " + targetFolderImages.getAbsolutePath());
             FileUtils.copyFile(imageFile, new File(targetFolderImages, String.format("%08d", physicalPageNumber) + image_file_suffiix_to_use));
             //FileUtils.copyFile(imageFile, new File(targetFolderImages, StringUtils.leftPad(issueNumber, 2, "0") + "_" + String.format("%08d", physicalPageNumber) + IMAGE_FILE_SUFFIX_TO_USE));
 
             // no matter if new or current issue, add now all pages to current issue
-            if (issue!=null){
+            if (issue != null) {
 
                 // assigning page numbers
                 DocStructType newPage = prefs.getDocStrctTypeByName("page");
@@ -445,7 +439,7 @@ public class BSZ_BodenseeImport_Helper {
                 addMetadata(dsPage, "physPageNumber", String.valueOf(physicalPageNumber++));
                 addMetadata(dsPage, "logicalPageNumber", element.getLabel());
                 volume.addReferenceTo(dsPage, "logical_physical");
-                if (createIssues){
+                if (createIssues) {
                     issue.addReferenceTo(dsPage, "logical_physical");
                 }
 
@@ -469,26 +463,26 @@ public class BSZ_BodenseeImport_Helper {
      * 
      * @throws IOException
      */
-    private List<BSZ_BodenseeImport_Element> readElementsFromSql(String inYearAndIssue) throws IOException{
+    private List<BSZ_BodenseeImport_Element> readElementsFromSql(String inYearAndIssue) throws IOException {
         List<BSZ_BodenseeImport_Element> elements = new ArrayList<>();
         // read all elements from the sql file
         try {
-            File f = new File (bsz_import_sql_file);
+            File f = new File(bsz_import_sql_file);
             List<String> lines = FileUtils.readLines(f, "UTF-8");
             for (String line : lines) {
-                if (line.startsWith("INSERT INTO")){
+                if (line.startsWith("INSERT INTO")) {
                     BSZ_BodenseeImport_Element element = convertSqlToElement(line);
 
                     String isn = "";
-                    if (StringUtils.isNumeric(element.getIssueNumber())){
+                    if (StringUtils.isNumeric(element.getIssueNumber())) {
                         isn = "_" + element.getIssueNumber();
                     }
 
                     String comparer = element.getJahr() + isn;
-                    if (separateBookletIds){
+                    if (separateBookletIds) {
                         comparer += "_" + element.getBookletid();
                     }
-                    if (comparer.equals(inYearAndIssue)){
+                    if (comparer.equals(inYearAndIssue)) {
                         elements.add(element);
                     }
                 }
@@ -503,10 +497,9 @@ public class BSZ_BodenseeImport_Helper {
     }
 
     /**
-     * Method to create or to adapt an identifier for a given structural element. The identifier that is
-     * named as parameter 'field' is searched in the structural element and gets the prefix 'bsz' added if
-     * it is not there already. In case the identifier is not existing add it now to the structural element
-     * incl. the prefix 'bsz'
+     * Method to create or to adapt an identifier for a given structural element. The identifier that is named as parameter 'field' is searched in the
+     * structural element and gets the prefix 'bsz' added if it is not there already. In case the identifier is not existing add it now to the
+     * structural element incl. the prefix 'bsz'
      * 
      * @param ds the structural element to use
      * @param field the metadata field that is added or adapted
@@ -514,14 +507,14 @@ public class BSZ_BodenseeImport_Helper {
      * 
      * @throws MetadataTypeNotAllowedException
      */
-    private void adaptIdentifier(DocStruct ds, String field, String value) throws MetadataTypeNotAllowedException{
+    private void adaptIdentifier(DocStruct ds, String field, String value) throws MetadataTypeNotAllowedException {
         List<? extends Metadata> mdlist = ds.getAllMetadataByType(prefs.getMetadataTypeByName(field));
-        if (mdlist != null && mdlist.size()>0){
+        if (mdlist != null && mdlist.size() > 0) {
             Metadata md = mdlist.get(0);
-            if (md!=null &&  !md.getValue().startsWith("bsz")){
-                md.setValue("bsz" +  md.getValue());
+            if (md != null && !md.getValue().startsWith("bsz")) {
+                md.setValue("bsz" + md.getValue());
             }
-        }else {
+        } else {
             Metadata md2 = new Metadata(prefs.getMetadataTypeByName(field));
             md2.setValue("bsz" + value);
             ds.addMetadata(md2);
@@ -537,7 +530,7 @@ public class BSZ_BodenseeImport_Helper {
      * 
      * @throws MetadataTypeNotAllowedException
      */
-    private void addMetadata(DocStruct ds, String field, String value) throws MetadataTypeNotAllowedException{
+    private void addMetadata(DocStruct ds, String field, String value) throws MetadataTypeNotAllowedException {
         Metadata mdColl = new Metadata(prefs.getMetadataTypeByName(field));
         mdColl.setValue(value);
         ds.addMetadata(mdColl);
